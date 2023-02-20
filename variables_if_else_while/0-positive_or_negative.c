34a4991aa24cf959dbb75fdf4b924ef9ec5ce4dd
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-02-20 20:42:38 +0000

    0 positive negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Description: 'using printf instead of puts'
+ * Description: 'if, else if, else'
  *
  * Return: Always 0 (Success)
  */
@@ -16,17 +16,21 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-	  printf("%d is positive\n" ,n);
+	  printf("%d is positive\n", n);
 	}
+
 	else if (n < 0)
 	{
-	  printf("%d is negative\n" ,n);
+	  printf("%d is negative\n", n);
 	}
+
 	else
 	{
-	  printf("%d is zero\n" ,n);
+	  printf("%d is zero\n", n);
 	}
+
 	return (0);
 }
