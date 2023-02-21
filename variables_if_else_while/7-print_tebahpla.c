0a8f0f3e4be4226b7ed8111531606984ded4ad13
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-02-21 15:16:33 +0000

    alphabet reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char f;
+
+	for (f = 'z'; f >= 'a'; f--)
+	{
+		putchar(f);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
