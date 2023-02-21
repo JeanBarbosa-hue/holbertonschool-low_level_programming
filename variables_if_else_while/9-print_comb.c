80f27e74b34b8afdd5f1244103e3538a89e61b26
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-02-21 17:23:14 +0000

    combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = '0'; g <= '9'; g++)
+	{
+		putchar(g);
+
+		if (g != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
