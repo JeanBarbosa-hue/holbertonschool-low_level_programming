517e6f72208e2f61659f8cfeb76d7ad598604905
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-02-23 19:06:53 +0000

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 
 /**
- *  print_last_digit - print last digit
+ *print_last_digit - print last digit
  *
- *   print_last_digit
- *
- *
+ *Return: x
  *
  */
 
@@ -14,7 +12,7 @@ int print_last_digit(int p)
 {
 	int x = p % 10;
 
-	if (x < 0)  
+	if (x < 0) 
 	{
 		x = x * -1;
 	}
