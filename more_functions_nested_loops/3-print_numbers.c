cf1394a1b705f8db88ea86e18f7a866c4c98cf14
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-02-25 03:52:06 +0000

    print num

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ *@d: variable to be checked
+ */
+
+void print_numbers(void)
+{
+	int r;
+
+	for ( r = 0; r <= 9; r++)
+	{
+		putchar(r + '0');
+	}
+	putchar('\n');
+}
