11285097701777a3f65d13714f787d8a763a9fa0
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-04 19:31:44 +0000

    strchr

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory
+ *
+ * @
+ * @
+ * @ 
+ *
+ *  Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int a;
+    char *csrc = (char *)src;
+    char *cdest = (char *)dest;
+
+    for (a = 0; a < n; a++)
+    {
+        cdest[a] = csrc[a];
+    }
+
+    return dest;
+}
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character
+ *
+ * @
+ * @
+ *
+ * Return:
+ */
+
+char *_strchr(char *s, char c)
+{
+    int a = 0, b;
+
+    while (s[a])
+    {
+        a++;
+    }
+
+    for (b = 0; b <= a; b++)
+    {
+        if (c == s[b])
+        {
+            s += b;
+            return s;
+        }
+    }
+
+    return ('\0');
+}
