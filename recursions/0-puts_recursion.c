523a82c3f83c0889277f66d5bff3d3579e4321a4
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-07 15:00:18 +0000

    put recursion

---
+++ recursions/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - string length
+ *
+ * @s: pointer
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
+
+	else
+	{
+	putchar(*s);
+	_puts_recursion(s + 1);
+	}
+}
