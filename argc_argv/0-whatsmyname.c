3098a30a1e874170736ea2b20ac12595ced60b43
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-09 20:56:24 +0000

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: variable 
+ * @argv: pointer
+ *
+ * Return: 0
+ */
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf ("%s\n", argv[0]);
+	
+	return (0);
+}
