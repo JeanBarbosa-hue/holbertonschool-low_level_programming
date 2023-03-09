85407d6679c8a00f94e284c5c8a5a13ef592bc32
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-09 23:46:29 +0000

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: variable
+ * @argv: pointer/array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
