ea4e74986e2709f68e68dffcd40c35c8300c1feb
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-10 00:59:39 +0000

    mul and sum

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - Entry point
+ *
+ * @argc: variable
+ * @argv: pointer/array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+
+	int res = a * b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", res);
+
+	return (0);
+}
