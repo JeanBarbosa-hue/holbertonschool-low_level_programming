70ae81f70437e576bd598e43b67004cd8966987e
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-11 02:20:10 +0000

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - Entry point
@@ -13,32 +12,32 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int sum = 0;
+	int i, j, sum;
 
-    for (i = 1; i < argc; i++) 
-    {
-        for (j = 0; argv[i][j] != '\0'; j++) 
+	for (i = 1; i < argc; i++)
 	{
-            if (!isdigit(argv[i][j])) 
-	    {
-                printf("Error\n");
-                return 1;
-            }
-        }
 
-	int num = atoi(argv[i]);
-
-	if (num < 0) 
+		for (j = 0; argv[i][j]; j++)
 	{
-            printf("Error\n");
-            return 1;
-        }
-        
-	sum += num;
-    }
 
-    printf("%d\n", sum);
+		if (argv[i][j] < '0' || argv[i][j] > '9')
+	{
 
-    return 0;
+
+		printf("Error\n");
+
+		return (1);
+
+	}
+
+	}
+
+
+		sum += atoi(argv[i]);
+
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
