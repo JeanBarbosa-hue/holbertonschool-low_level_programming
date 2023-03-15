44b738d759fb2866425ecdb4262f2224667a271c
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-15 01:46:41 +0000

    alloc grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - return 2 dimensional array
+ *
+ * @width: integer
+ * @height: integer
+ *
+ * Return: dup
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+	int **dup;
+	int hgt_index, wid_index;
+
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	dup = malloc(sizeof(int *) * height);
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		dup[hgt_index] = malloc(sizeof(int) * width);
+
+	if (dup[hgt_index] == NULL)
+	{
+		for (; hgt_index >= 0; hgt_index--)
+			free(dup[hgt_index]);
+			free(dup);
+	return (NULL);
+	}
+
+	}
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+
+		for (wid_index = 0; wid_index < width; wid_index++)
+
+			dup[hgt_index][wid_index] = 0;
+	}
+
+	return (dup);
+}
