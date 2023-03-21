784da696975577cf178d1c6e2a784b170c8bbe1f
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-21 15:16:42 +0000

    dog part 2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,8 +17,11 @@ struct dog
 	char *name;
 	char *owner;
 	float age;
+
+
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 #endif
