6ea2fb394d019302719ce2f92bf25f753a06adb5
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-03-22 04:21:45 +0000

    free

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,21 +1,16 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * free_dog - free space for dog
  *
- * d: pointer
+ * @d: receives pointer
+ *
  */
 
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-	{
-		return;
-	}
-
-	free(d->name);
 	free(d->owner);
+	free(d->name);
 	free(d);
-
 }
