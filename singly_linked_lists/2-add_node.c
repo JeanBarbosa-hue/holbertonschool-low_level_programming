f711f44597758e5243cb3256e1b09c0f7eb18bef
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-04-05 01:39:47 +0000

    creating new node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+	unsigned int a;
+
+	if (head == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
+	node -> str = strdup(str);
+
+	if (node -> str == NULL)
+	{
+		free(node);
+		return(NULL);
+	}
+
+	for (a = 0; str[a] != '\0'; a++);
+
+	node -> len = a;
+	node -> next = *head;
+	*head = node;
+
+	return (node);
+}
