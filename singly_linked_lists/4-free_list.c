5380056899d325192c03eae7ab1870ea36bf8b83
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-04-05 02:29:41 +0000

    free list_t

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free list_t list
+ *
+ * @head: pointer to list_t
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
